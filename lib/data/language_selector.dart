989d193f369cd299d44bbe21c3ddbe0a1d0a1a85
August Aublet <august.aublet@gmail.com>
2022-12-16 10:37:02 +0100

    cleanup

--- lib/data/language_selector.dart
+++ lib/data/language_selector.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
-
+import 'dart:ffi';
 import 'package:template/data/open_ai_translater.dart';
 import 'package:template/data/question.dart';
-import 'package:template/data/libre_translator.dart';
 import 'package:translator/translator.dart';
 
 class TranslatorTesting {
@@ -11,55 +10,27 @@ class TranslatorTesting {
 
   Future<void> translatQuestion(Question question,
       {String targetLanguage = "sv"}) async {
-    LibreTranslateAPI tranlator = LibreTranslateAPI();
-    GoogleTranslator googleTranslator = GoogleTranslator();
     TranslateOpenAI translateOpenAI = TranslateOpenAI();
 
-    List queryList = [];
-    queryList.add(question.question);
-    queryList.add(question.correctAnswer);
-    for (String i in question.incorrectAnswer) {
-      queryList.add(i);
-    }
-    // var questionJson = jsonEncode({
-    //   "question": question.question,
-    //   "correctAnswer": question.correctAnswer,
-    //   "incorrectAnswer": jsonEncode(question.incorrectAnswer)
-    // });
-
+    // Creating a Json format string containing the strings that is going to be translated.
     var questionJson = jsonEncode({
       "1": question.question,
       "2": question.correctAnswer,
       "3": jsonEncode(question.incorrectAnswer)
     });
 
-    // Original string and print
-    String csvTranslationString =
-        '${question.question};${question.correctAnswer};${question.incorrectAnswer[0]};${question.incorrectAnswer[1]};${question.incorrectAnswer[2]};';
-    // print("Original ;$csvTranslationString");
-
-    // Stopwatch librestopwatch = new Stopwatch()..start();
-    // var translatedQuestionLibreCSV =
-    //     await tranlator.translate(q: csvTranslationString, target: "sv");
-    // librestopwatch.stop();
-
-    // var translatedQuestionLibre =
-    //     await tranlator.translate(q: queryList, target: "sv");
-
-    Stopwatch googlestopwatch = new Stopwatch()..start();
-    var translatedQuestionGoogle = await googleTranslator
-        .translate(csvTranslationString, from: "en", to: "sv");
-    googlestopwatch.stop();
-
-    Stopwatch openAIstopwatch = new Stopwatch()..start();
     var translatedQuestionOpenAI =
         await translateOpenAI.getTranslate(questionJson);
-    openAIstopwatch.stop();
-    // Map<dynamic, dynamic> test = jsonDecode(translatedQuestionopenAI);
-    List incorrectTranslated = jsonDecode(translatedQuestionOpenAI["3"]);
-    String translatedOpenAI =
-        "${translatedQuestionOpenAI["1"]},${translatedQuestionOpenAI["2"]},${incorrectTranslated[0]},${incorrectTranslated[1]},${incorrectTranslated[2]}";
 
+    // Map<dynamic, dynamic> test = jsonDecode(translatedQuestionopenAI);
+    // List incorrectTranslated = jsonDecode(translatedQuestionOpenAI["3"]);
+    // String translatedOpenAI =
+    //     "${translatedQuestionOpenAI["1"]},${translatedQuestionOpenAI["2"]},${incorrectTranslated[0]},${incorrectTranslated[1]},${incorrectTranslated[2]}";
+
+    question.updateWithTranslation(
+        translatedQuestionOpenAI["1"],
+        translatedQuestionOpenAI["2"],
+        jsonDecode(translatedQuestionOpenAI["3"]));
     // List templist = [];
     // test.forEach((k, v) => templist.add(v));
 
@@ -79,9 +50,5 @@ class TranslatorTesting {
     //     "LibreTranslateCSV; Time: ${librestopwatch.elapsed.toString()} ;$translatedQuestionLibreCSV");
     //print(
     //    "LibreTranslate; Time: ${librestopwatch.elapsed.toString()};$translatedQuestionLibre");
-    print(
-        "Google; Time: ${googlestopwatch.elapsed.toString()} ;$translatedQuestionGoogle");
-    print(
-        "OpenAI; Time: ${openAIstopwatch.elapsed.toString()}; $translatedOpenAI");
   }
 }
