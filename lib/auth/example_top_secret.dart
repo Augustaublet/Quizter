e6fc648405df594a2c150578a9d8614bdb23aae9
August Aublet <august.aublet@gmail.com>
2023-01-09 13:29:22 +0100

    uppdate example_top_secret With OpenAI

--- lib/auth/example_top_secret.dart
+++ lib/auth/example_top_secret.dart
@@ -8,4 +8,7 @@
 // Also during testin change dbName in hiscores.dart to "highscores-1"
 
 var databaseKey = "Your secret key here";
+
+// to use this funktion you need to insert your OpenAI API key here.
+// Create an Account at https://auth0.openai.com/u/signup/identifier?state=hKFo2SBBYVAzZ0ZOXzR0MnZhdDU3QURpbEYxSFZlY1ZaTFFlbaFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDcyODFvcTBnelY5SzdaZ2RZUmt1NTRzRzBmQWtpOUxSo2NpZNkgRFJpdnNubTJNdTQyVDNLT3BxZHR3QjNOWXZpSFl6d0Q
 var openAIKey = "Your secret key here";
